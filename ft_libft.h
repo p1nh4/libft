e0e8ad1b8988b63ee799e1e3695caf422783eff7
p1nh4 <p1nh4@protonmail.com>
2025-10-14 00:28:24 +0100

    primeiras funcoes criadas libft.h

---
+++ ft_libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_libft.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidos- <davidos-@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 15:57:27 by davidos-          #+#    #+#             */
+/*   Updated: 2025/10/13 20:28:23 by davidos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+
+#endif
---
+++ teste_functions.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teste_functions.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidos- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 20:04:46 by davidos-          #+#    #+#             */
+/*   Updated: 2025/10/13 20:48:13 by davidos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_TESTE_FUNCTIONS
+# define FT_TESTE_FUNCTIONS
+
+int	teste_isdigit(void);
+int	teste_isalnum(void);
+int	teste_isalpha(void);
+int	teste_isascii(void);
+
+#endif 
